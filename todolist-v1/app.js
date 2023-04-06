f4e50e14f28fa21d30c3f48737f6a2c8c3396e7c
JAEE <jaeework@gmail.com>
2023-04-06 21:53:13 +0900

    Update todolist_Connect to MongoDB
    connect to MongoDB and implement insert

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -1,55 +1,117 @@
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+const mongoose = require("mongoose");
+//const date = require(__dirname + "/date.js");
 
 const app = express();
 
-const items = ["Buy Food", "Cook Food", "Eat Food"];    // It's possible to push items into the constant array. Assigning a new array to the variable throws an error
-const workItems = [];
+// const items = ["Buy Food", "Cook Food", "Eat Food"];    // It's possible to push items into the constant array. Assigning a new array to the variable throws an error
+// const workItems = [];
+
 
 app.set("view engine", "ejs");  // for using ejs, need to make a folder named 'views' and ejs file under the folder.
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+
+// The URL that we're going to connect where our MongoDB is hosted locally
+// todolistDB : the name of our database
+mongoose.connect("mongodb://localhost:27017/todolistDB", { useNewUrlParser: true });
+
+// create Schema
+/*
+  const <schemaName> = {
+    <fieldName> : <FieldDataType>,
+  };
+ 
+ */
+const itemSchema = {
+    name: String
+};
+
+// create model base on the schema
+//const = mongoose.model("SingularCollectionName", <schemaName>);
+const Item = mongoose.model("Item", itemSchema);
+
+/* const <constantName> = new <ModelName>({
+    <fieldName> : <fieldData>,
+}); */
+const item1 = new Item({ name: "Welcome to your todolist!" });
+const item2 = new Item({ name: "Hit the + button to add a new item." });
+const item3 = new Item({ name: "<-- Hit this to delete an item." });
+
+const defaultItems = [item1, item2, item3];
+
 
 
 /* GET */
 // root route
-app.get("/", function(req, res){
+app.get("/", function (req, res) {
 
-    const day = date.getDate();
+    //const day = date.getDate();
 
-    res.render("list", {listTitle : day, newListItems : items});    // render method uses the view engine that we set up to render a particular page.
-    
+    /* Mongoose find()
+        <ModelName>.find({conditions}, function(err, results){
+        });
+    */
+    Item.find({}).then(function (foundItems) {
+        console.log(foundItems);
+        if (foundItems.length == 0) {
+            Item.insertMany(defaultItems).then(function () {
+                console.log("Successfully saved defulat items to DB.");
+            }).catch(function (err) {
+                console.log(err);
+            });
+            res.redirect("/");
+        } else {
+            // render method uses the view engine that we set up to render a particular page.
+            res.render("list", { listTitle: "Today", newListItems: foundItems });
+        }
+
+    }).catch(function (err) {
+        console.log(err);
+    });
+
+
+
 });
 
 // work route
-app.get("/work", function(req, res){
-    res.render("list", {listTitle: "Work List", newListItems : workItems});
+app.get("/work", function (req, res) {
+    res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
 // about route
-app.get("/about", function(req, res) {
+app.get("/about", function (req, res) {
     res.render("about");
 });
 
 
 /* POST */
-app.post("/", function(req, res) {
-    const item = req.body.newItem;
+app.post("/", function (req, res) {
+    const itemName = req.body.newItem;
 
-    if (req.body.list === "Work") {
+    const item = new Item({
+        name : itemName
+    });
+
+    item.save();
+
+    res.redirect("/");
+
+    /* if (req.body.list === "Work") {
         workItems.push(item);
         res.redirect("/work");
     } else {
         items.push(item);
         res.redirect("/");
-    }
-    
+    } */
+
 });
 
 
-app.listen(3000, function(){
+app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
